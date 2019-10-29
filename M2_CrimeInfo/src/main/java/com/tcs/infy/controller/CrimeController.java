1a58270ee60a47148f6f46a3a9eb02c80a9313f9
ilankumaran11 <ilankumaran.i@gmail.com>
2019-10-29 06:52:36 +0530

    Adding VO and swagger annotation

--- M2_CrimeInfo/src/main/java/com/tcs/infy/controller/CrimeController.java
+++ M2_CrimeInfo/src/main/java/com/tcs/infy/controller/CrimeController.java
@@ -7,6 +7,10 @@ import java.util.stream.Collectors;
 import com.tcs.infy.entity.Crime;
 import com.tcs.infy.service.CrimeService;
 import common.tcs.infy.mapper.response.vo.CrimeVo;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -21,39 +25,67 @@ public class CrimeController extends VoMapper{
 	CrimeService accusedService;
 
 
-		//http://<HOSTNAME>:<PORT>/crime/name/<NAME>/ilan.brio
+	@ApiOperation(value = "Fetch Crime by Name", notes="fetch Crime Info",nickname = "Crime Info")
+	@ApiResponses(value = {
+			@ApiResponse(code = 500, message = "Server error !!!"),
+			@ApiResponse(code = 404, message = "Service not found !!!"),
+			@ApiResponse(code = 401, message = "Unauthorized access !!!"),
+			@ApiResponse(code = 403, message = "Forbidden access !!!"),
+			@ApiResponse(code = 200, message = "Successful retrieval !!!", responseContainer = "List of Crime", response = CrimeVo[].class) })
 		@GetMapping(value = "/name/{name}/ilan.brio",produces = MediaType.APPLICATION_JSON_VALUE)
-		public List<CrimeVo> findByAccusedName(@PathVariable("name") String name)
+		public List<CrimeVo> findByAccusedName(@ApiParam(value = "Accused name as input", required = true, defaultValue = "ILAN") @PathVariable("name") String name)
 		{
 			return accusedService.findByAccusedName(name).stream().map(p->{return entityToVo.apply(p);}).collect(Collectors.toList());
 		}
-	
-		//http://<HOSTNAME>:<PORT>/crime/count/<COUNT>/ilan.brio	
+
+	@ApiOperation(value = "Fetch Crime Count greater than ?", notes="fetch Crime Info",nickname = "Crime Info")
+	@ApiResponses(value = {
+			@ApiResponse(code = 500, message = "Server error !!!"),
+			@ApiResponse(code = 404, message = "Service not found !!!"),
+			@ApiResponse(code = 401, message = "Unauthorized access !!!"),
+			@ApiResponse(code = 403, message = "Forbidden access !!!"),
+			@ApiResponse(code = 200, message = "Successful retrieval !!!", responseContainer = "List of Crime", response = CrimeVo[].class) })
 	 	@GetMapping(value = "/count/{countNo}/ilan.brio",produces = MediaType.APPLICATION_JSON_VALUE)
-	    public List<CrimeVo> crimeCountGreaterThan(@PathVariable("countNo") int count)
+	    public List<CrimeVo> crimeCountGreaterThan(@ApiParam(value = "Crime Count as input", required = true, defaultValue = "2") @PathVariable("countNo") int count)
 	    {
 	 		return accusedService.crimeCountGreaterThan(count).stream().map(p->{return entityToVo.apply(p);}).collect(Collectors.toList());
 	    }
-	 	
 
 
-	 	//http://<HOSTNAME>:<PORT>/crime/name/count/ilan.brio?accusedName=<ACCUSED_NAME>&count=<ARRESTED_COUNT>
+
+	@ApiOperation(value = "Fetch Crime Count greater than ?", notes="fetch Crime Info",nickname = "Crime Info")
+	@ApiResponses(value = {
+			@ApiResponse(code = 500, message = "Server error !!!"),
+			@ApiResponse(code = 404, message = "Service not found !!!"),
+			@ApiResponse(code = 401, message = "Unauthorized access !!!"),
+			@ApiResponse(code = 403, message = "Forbidden access !!!"),
+			@ApiResponse(code = 200, message = "Successful retrieval !!!", responseContainer = "List of Crime", response = CrimeVo[].class) })
 	 	@GetMapping(value = "/name/count/ilan.brio",produces = MediaType.APPLICATION_JSON_VALUE)
-	    public List<CrimeVo> findByAccusedNameAndCrimeCount(@RequestParam("accusedName") String name,@RequestParam(value = "arrestedCount") int count)
+	    public List<CrimeVo> findByAccusedNameAndCrimeCount(@ApiParam(value = "Accused name as input", required = true, defaultValue = "ILAN")  @RequestParam("accusedName") String name, @ApiParam(value = "Crime Count as input", required = true, defaultValue = "1") @RequestParam(value = "arrestedCount") int count)
 	    {
     		return accusedService.findByAccusedNameAndCrimeCount(name,count).stream().map(p->{return entityToVo.apply(p);}).collect(Collectors.toList());
 	    }
-	 	
-	 	
-	 	//http://<HOSTNAME>:<PORT>/save/all/crime/ilan.brio
+
+
+	@ApiResponses(value = {
+			@ApiResponse(code = 500, message = "Server error !!!"),
+			@ApiResponse(code = 404, message = "Service not found !!!"),
+			@ApiResponse(code = 401, message = "Unauthorized access !!!"),
+			@ApiResponse(code = 403, message = "Forbidden access !!!"),
+			@ApiResponse(code = 200, message = "Successful retrieval !!!", responseContainer = "List of Crime", response = CrimeVo[].class) })
 	 	@PostMapping(value = "/save/all/crime/ilan.brio",produces = MediaType.APPLICATION_JSON_VALUE)
 	    public List<CrimeVo> saveAllCrime(@RequestBody List<Crime> accused)
 	    {
     		return accusedService.saveAll(accused).stream().map(p->{return entityToVo.apply(p);}).collect(Collectors.toList());
 	    }
-	 	
-	 	
-	 	//http://<HOSTNAME>:<PORT>/save/only/one/crime/ilan.brio
+
+
+	@ApiResponses(value = {
+			@ApiResponse(code = 500, message = "Server error !!!"),
+			@ApiResponse(code = 404, message = "Service not found !!!"),
+			@ApiResponse(code = 401, message = "Unauthorized access !!!"),
+			@ApiResponse(code = 403, message = "Forbidden access !!!"),
+			@ApiResponse(code = 200, message = "Successful retrieval !!!", responseContainer = "List of Crime", response = CrimeVo.class) })
 	 	@PostMapping(value = "/save/only/one/crime/ilan.brio",produces = MediaType.APPLICATION_JSON_VALUE)
 	    public CrimeVo saveCrime(@RequestBody Crime accused)
 	    {
