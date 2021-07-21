aa7d126a358b37c7f77f5085201187b201def1c6
ilankumaran11 <ilankumaran.i@gmail.com>
2021-07-21 15:06:33 +0530

    WebMvcAdapter to WebMvcConfigurer

--- M1_AccusedInfo/src/main/java/com/ilan/interceptors/InterceptorConfig.java
+++ M1_AccusedInfo/src/main/java/com/ilan/interceptors/InterceptorConfig.java
@@ -3,10 +3,11 @@ package com.ilan.interceptors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class InterceptorConfig extends WebMvcConfigurerAdapter {
+public class InterceptorConfig implements WebMvcConfigurer {
 
     @Autowired
     LoggerInterceptor logInterceptor;
