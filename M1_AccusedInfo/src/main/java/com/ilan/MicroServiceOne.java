8f8dbd9959f6c3273b95f992df19365c95039192
ilankumaran11 <ilankumaran.i@gmail.com>
2021-08-26 19:58:48 +0530

    Configuration file ordering

--- M1_AccusedInfo/src/main/java/com/ilan/MicroServiceOne.java
+++ M1_AccusedInfo/src/main/java/com/ilan/MicroServiceOne.java
@@ -1,5 +1,7 @@
 package com.ilan;
 
+import com.ilan.configuration.LogAspect;
+import com.ilan.configuration.RestTemplateConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -18,7 +20,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class MicroServiceOne extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroServiceOne.class, args);
+		SpringApplication.run(new Class[]{MicroServiceOne.class, RestTemplateConfig.class, LogAspect.class}, args);
 	}
 
 	@Override
